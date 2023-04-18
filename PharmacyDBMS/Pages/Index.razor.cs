44b88f239de8e6918ab71884b1316a14e0a3c2fb
ritwikan <117129734+ritwikan@users.noreply.github.com>
2023-04-18 14:35:23 -0600

    yeehaw

--- PharmacyDBMS/Pages/Index.razor.cs
+++ PharmacyDBMS/Pages/Index.razor.cs
@@ -1,9 +1,18 @@
-﻿using PharmacyDBMS.Data;
+﻿using Microsoft.AspNetCore.Components;
+using PharmacyDBMS.Data;
+using System.ComponentModel.DataAnnotations;
+
+
 
 namespace PharmacyDBMS.Pages
 {
     public partial class Index
     {
-     
+        public int InputValue { get; set; }
+
+        public string password = "login123";
+
+
+      
     }
 }
